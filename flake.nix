995cbb1ba69bb315c3705a36b999f48da247bc5a
l0rem <nikita@fastdocs.de>
2025-04-01 01:06:35 +0200

    Add flake.lock and update flake.nix to configure nix-darwin and nixpkgs inputs, enhance system package management, and enable Homebrew support.

--- flake.nix
+++ flake.nix
@@ -3,62 +3,92 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      #TODO: pulltube, cleanshotX, windscribe
-      environment.systemPackages =
-        [
+    let
+      configuration = { pkgs, lib, ... }: {
+        nix.enable = false;
+
+         nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) 
+         [
+          "arc-browser"
+          # "docker"
+          "google-chrome"
+          "cursor"
+          "postman"
+          "discord"
+          "teams"
+          # "lmstudio"
+          "vscode"
+          "raycast"
+          "soundsource"
+          # "Xcode.app"
+          "bartender"
+         ];
+        # System packages via Nix
+        environment.systemPackages = [
           pkgs.arc-browser
           pkgs.docker
-          pkgs.ghostty
+          # pkgs.ghostty
           pkgs.unnaturalscrollwheels
           pkgs.sqlitebrowser
-          pkgs.visual-studio-code
           pkgs.google-chrome
           pkgs.code-cursor
           pkgs.postman
           pkgs.discord
-          pkgs.teams
-          pkgs.lmstudio
-          pkgs.obs-studio
+          # pkgs.teams
+          # pkgs.lmstudio
+          # pkgs.obs-studio
           pkgs.ollama
           pkgs.jetbrains.pycharm-community-bin
           pkgs.pgadmin4
           pkgs.telegram-desktop
           pkgs.vscode
-          pkgs.vlc
-          pkgs.handbrake
+          # pkgs.vlc
+          # pkgs.handbrake
           pkgs.python313
           pkgs.postgresql_15
           pkgs.raycast
           pkgs.soundsource
-          pkgs.darwin.xcode
+          # pkgs.darwin.xcode
           pkgs.zsh-autosuggestions
           pkgs.zsh-completions
           pkgs.git
           pkgs.docker-compose
+          pkgs.bartender
         ];
 
-      programs.homebrew = {
-        enable = true;
-        casks = [
-          "keepingyouawake"
-    ];
-  };
-      nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 6;
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # Homebrew support
+        homebrew = {
+          enable = true;
+          casks = [
+            "keepingyouawake"
+            "lm-studio"
+          ];
+          # Optional:
+          # onActivation.cleanup = "uninstall"; # cleans up unlisted brews/casks
+        };
+
+        # Touch ID for sudo
+        security.pam.services.sudo_local.touchIdAuth = true;
+
+        # Nix config
+        nix.settings.experimental-features = "nix-command flakes";
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 6;
+
+        # Platform
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+    {
+      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
