3b4a123fb30a9c5a431e03a390a2d87a13743807
l0rem <nikita@fastdocs.de>
2025-04-01 14:43:46 +0200

    Enhance flake.nix by adding MongoDB and Redis services, updating system packages, and refining Homebrew casks for improved functionality and user experience.

--- flake.nix
+++ flake.nix
@@ -9,81 +9,120 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
     let
-      configuration = { pkgs, lib, ... }: {
-        nix.enable = false;
+      configuration =
+        { pkgs, lib, ... }:
+        {
+          nix.enable = false;
 
-         nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) 
-         [
-          "arc-browser"
-          "google-chrome"
-          "cursor"
-          "postman"
-          "discord"
-          "teams"
-          "vscode"
-          "raycast"
-          "soundsource"
-          "bartender"
-         ];
-        # System packages via Nix
-        environment.systemPackages = [
-          pkgs.arc-browser
-          pkgs.docker
-          pkgs.unnaturalscrollwheels
-          pkgs.sqlitebrowser
-          pkgs.google-chrome
-          pkgs.code-cursor
-          pkgs.postman
-          pkgs.discord
-          pkgs.ollama
-          pkgs.jetbrains.pycharm-community-bin
-          pkgs.pgadmin4
-          pkgs.telegram-desktop
-          pkgs.vscode
-          pkgs.python313
-          pkgs.postgresql_15
-          pkgs.raycast
-          pkgs.soundsource
-          # pkgs.darwin.xcode
-          pkgs.zsh-autosuggestions
-          pkgs.zsh-completions
-          pkgs.git
-          pkgs.docker-compose
-          pkgs.bartender
-        ];
+          nixpkgs.config.allowUnfreePredicate =
+            pkg:
+            builtins.elem (lib.getName pkg) [
+              "arc-browser"
+              "google-chrome"
+              "cursor"
+              "postman"
+              "discord"
+              "teams"
+              "vscode"
+              "raycast"
+              "soundsource"
+              "bartender"
+              "mongodb-ce"
+            ];
 
-        # Homebrew support
-        homebrew = {
-          enable = true;
-          casks = [
-            "keepingyouawake"
-            "lm-studio"
-            "handbrake"
-            "vlc"
-            "obs"
-            "docker"
-            "microsoft-teams"
-            "ghostty"
-            "cleanshot"
-            "windscribe"
+          environment.systemPackages = [
+            pkgs.arc-browser
+            pkgs.docker
+            pkgs.unnaturalscrollwheels
+            pkgs.sqlitebrowser
+            pkgs.google-chrome
+            pkgs.code-cursor
+            pkgs.postman
+            pkgs.discord
+            pkgs.ollama
+            pkgs.jetbrains.pycharm-community-bin
+            pkgs.pgadmin4
+            pkgs.telegram-desktop
+            pkgs.vscode
+            pkgs.python313
+            pkgs.postgresql_15
+            pkgs.raycast
+            pkgs.soundsource
+            pkgs.zsh-autosuggestions
+            pkgs.zsh-completions
+            pkgs.git
+            pkgs.docker-compose
+            pkgs.bartender
+            pkgs.redis
+            pkgs.nixfmt-rfc-style
+            pkgs.mongodb-ce
           ];
-          # Optional:
-          # onActivation.cleanup = "uninstall"; # cleans up unlisted brews/casks
+
+          homebrew = {
+            enable = true;
+            casks = [
+              "keepingyouawake"
+              "lm-studio"
+              "handbrake"
+              "vlc"
+              "obs"
+              "docker"
+              "microsoft-teams"
+              "ghostty"
+              "cleanshot"
+              "windscribe"
+              "obsidian"
+              "mongodb-compass"
+            ];
+          };
+
+          launchd.daemons.redis = {
+            serviceConfig = {
+              Label = "org.nixos.redis";
+              ProgramArguments = [ "${pkgs.redis}/bin/redis-server" ];
+              KeepAlive = true;
+              RunAtLoad = true;
+              StandardOutPath = "/tmp/redis.log";
+              StandardErrorPath = "/tmp/redis-error.log";
+            };
+          };
+
+          launchd.daemons.mongodb = {
+            serviceConfig = {
+              Label = "org.nixos.mongodb";
+              ProgramArguments = [
+                "${pkgs.mongodb-ce}/bin/mongod"
+                "--dbpath"
+                "/var/db/mongodb"
+              ];
+              KeepAlive = true;
+              RunAtLoad = true;
+              StandardOutPath = "/tmp/mongodb.log";
+              StandardErrorPath = "/tmp/mongodb-error.log";
+            };
+          };
+
+          system.activationScripts.postActivation.text = ''
+            echo "Running mongodbDataDir script" >> /tmp/mongo-script.log
+            mkdir -p /var/db/mongodb
+            chown "$(id -un)" /var/db/mongodb
+          '';
+
+          security.pam.services.sudo_local.touchIdAuth = true;
+
+          nix.settings.experimental-features = "nix-command flakes";
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = 6;
+
+          nixpkgs.hostPlatform = "aarch64-darwin";
         };
-
-        # Touch ID for sudo
-        security.pam.services.sudo_local.touchIdAuth = true;
-
-        # Nix config
-        nix.settings.experimental-features = "nix-command flakes";
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-        system.stateVersion = 6;
-
-        # Platform
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
     in
     {
       darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
